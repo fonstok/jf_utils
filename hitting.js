4e9506820bd545898a26c6a7080cb663789e270d
Jon Fahnestock <jnk@fonstok.com>
2020-10-21 16:45:27 -0500

    Update hitting.js

--- hitting.js
+++ hitting.js
@@ -1,4 +1,4 @@
-// pass this two selected objects and it will return a data object with the following information 
+// pass this two selected objects and it will return a data object with the following information
 /*
 hitting(object1, object2) // returns a data object with the following information
 {
@@ -9,46 +9,49 @@ hitting(object1, object2) // returns a data object with the following informatio
   hit:false // are the corners of the objects crossing?
  }    
 //*/
-
 let touchData = {
- left:false,
- right:false,
- top:false,
- bottom:false, 
- hit:false
-}
+  left: false,
+  right: false,
+  top: false,
+  bottom: false,
+  hit: false,
+};
 
-const hitting = function(_ob1, _ob2){
-    let _rect1 = _ob1.getBoundingClientRect();
-    let _rect2 =  _ob2.getBoundingClientRect();
-    
-    if (_rect1.x > _rect2.x && _rect1.x < _rect2.right) {
-      touchData.left = true;
-    } else {
-      touchData.left = false;
-    }
-    if (_rect1.right < _rect2.right && _rect1.right > _rect2.x) {
-      touchData.right = true;
-    } else {
-      touchData.right = false;
-    }
-    if (_rect1.y > _rect2.y && _rect1.y < _rect2.bottom){
-      touchData.top = true;
-    } else {
-      touchData.top = false;
-    }
-    if (_rect1.bottom < _rect2.bottom && _rect1.bottom > _rect2.y){
-      touchData.bottom = true;
-    } else {
-      touchData.bottom = false;
-    }
-    if ((touchData.left  || touchData.right)&&(touchData.top  || touchData.bottom)){
-      //console.log("hitting");
-      touchData.hit = true;
-    } else {
-      //console.log("not hitting");
-      touchData.hit = false;
-    }
-    //console.log(touchData);
-    return touchData; 
+function hitting(_ob1, _ob2) {
+  let _rect1 = _ob1.getBoundingClientRect();
+  let _rect2 = _ob2.getBoundingClientRect();
+
+  if (_rect1.x < _rect2.right && _rect1.x > _rect2.x) {
+    touchData.left = true;
+  } else {
+    touchData.left = false;
   }
+  if (_rect1.right < _rect2.right && _rect1.right > _rect2.x) {
+    touchData.right = true;
+  } else {
+    touchData.right = false;
+  }
+  if (_rect1.y > _rect2.y && _rect1.y < _rect2.bottom) {
+    touchData.top = true;
+  } else {
+    touchData.top = false;
+  }
+  if (_rect1.bottom < _rect2.bottom && _rect1.bottom > _rect2.y) {
+    touchData.bottom = true;
+  } else {
+    touchData.bottom = false;
+  }
+  if (
+    (touchData.left || touchData.right) &&
+    (touchData.top || touchData.bottom)
+  ) {
+    //console.log("hitting");
+    touchData.hit = true;
+  } else {
+    //console.log("not");
+    touchData.hit = false;
+  }
+
+  //console.log(touchData);
+  return touchData;
+}
